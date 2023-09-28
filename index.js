cbff4d1c2b3b0ea38d339a71daf5e59c9ddec7d6
Luis <luisdevv232@gmail.com>
2023-09-28 10:40:35 -0700

    agregando cambio que se va a revertir

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ console.log('soy el ejercicio 5');
 console.log('acabo de pasar a la version 2');
 
 console.log('acabo de ser introducido en la version 3, un hito importante');
+
+console.log('me van a revertir :(');
